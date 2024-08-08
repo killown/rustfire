c52a10d1901c2fba33ca0b252673733f149e29bf
killown <systemofdown@gmai.com>
2024-08-08 20:24:28 -0300

    added list output method

--- src/ipc.rs
+++ src/ipc.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::env;
-use std::io::{self};
+use std::io;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::UnixStream as TokioUnixStream;
 
@@ -8,15 +9,82 @@ use tokio::net::UnixStream as TokioUnixStream;
 pub struct MsgTemplate {
     pub method: String,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub data: Option<serde_json::Value>,
+    pub data: Option<Value>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct View {
-    pub id: i32,
-    pub role: String,
+    pub activated: bool,
+    #[serde(rename = "app-id")]
+    pub app_id: String,
+    #[serde(rename = "base-geometry")]
+    pub base_geometry: Geometry,
+    pub bbox: Geometry,
+    pub focusable: bool,
+    pub fullscreen: bool,
+    pub geometry: Geometry,
+    pub id: i64,
+    #[serde(rename = "last-focus-timestamp")]
+    pub last_focus_timestamp: i64,
+    pub layer: String,
     pub mapped: bool,
-    pub pid: i32,
+    #[serde(rename = "max-size")]
+    pub max_size: Size,
+    #[serde(rename = "min-size")]
+    pub min_size: Size,
+    pub minimized: bool,
+    #[serde(rename = "output-id")]
+    pub output_id: i64,
+    #[serde(rename = "output-name")]
+    pub output_name: String,
+    pub parent: i64,
+    pub pid: i64,
+    pub role: String,
+    pub sticky: bool,
+    #[serde(rename = "tiled-edges")]
+    pub tiled_edges: u64,
+    pub title: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    #[serde(rename = "wset-index")]
+    pub wset_index: u128,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Geometry {
+    pub height: u64,
+    pub width: u64,
+    pub x: u64,
+    pub y: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Size {
+    pub height: u64,
+    pub width: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Output {
+    pub geometry: Geometry,
+    pub id: i64,
+    pub name: String,
+    #[serde(rename = "workarea")]
+    pub work_area: Geometry,
+    #[serde(rename = "workspace")]
+    pub workspace: Workspace,
+    #[serde(rename = "wset-index")]
+    pub wset_index: u128,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Workspace {
+    #[serde(rename = "grid_height")]
+    pub grid_height: u64,
+    #[serde(rename = "grid_width")]
+    pub grid_width: u64,
+    pub x: u64,
+    pub y: u64,
 }
 
 pub struct WayfireSocket {
@@ -25,13 +93,13 @@ pub struct WayfireSocket {
 
 impl WayfireSocket {
     pub async fn connect() -> io::Result<Self> {
-        let socket_name = env::var("WAYFIRE_SOCKET")
-            .expect("WAYFIRE_SOCKET environment variable not set");
+        let socket_name =
+            env::var("WAYFIRE_SOCKET").expect("WAYFIRE_SOCKET environment variable not set");
         let client = TokioUnixStream::connect(&socket_name).await?;
         Ok(WayfireSocket { client })
     }
 
-    pub async fn send_json(&mut self, msg: &MsgTemplate) -> io::Result<serde_json::Value> {
+    pub async fn send_json(&mut self, msg: &MsgTemplate) -> io::Result<Value> {
         let data = serde_json::to_vec(msg)?;
         let header = (data.len() as u32).to_le_bytes();
 
@@ -47,12 +115,12 @@ impl WayfireSocket {
         Ok(buf)
     }
 
-    pub async fn read_message(&mut self) -> io::Result<serde_json::Value> {
+    pub async fn read_message(&mut self) -> io::Result<Value> {
         let len_buf = self.read_exact(4).await?;
         let len = u32::from_le_bytes(len_buf.try_into().unwrap()) as usize;
 
         let response_buf = self.read_exact(len).await?;
-        let response: serde_json::Value = serde_json::from_slice(&response_buf)?;
+        let response: Value = serde_json::from_slice(&response_buf)?;
 
         if response.get("error").is_some() {
             eprintln!("Error: {:?}", response);
@@ -61,7 +129,7 @@ impl WayfireSocket {
         Ok(response)
     }
 
-    pub async fn list_views(&mut self, filter_mapped_toplevel: bool) -> io::Result<Vec<View>> {
+    pub async fn list_views(&mut self) -> io::Result<Vec<View>> {
         let message = MsgTemplate {
             method: "window-rules/list-views".to_string(),
             data: None,
@@ -70,16 +138,19 @@ impl WayfireSocket {
         let response = self.send_json(&message).await?;
         let views: Vec<View> = serde_json::from_value(response)?;
 
-        let filtered_views = if filter_mapped_toplevel {
-            views
-                .into_iter()
-                .filter(|view| view.mapped && view.role == "toplevel")
-                .collect()
-        } else {
-            views
+        Ok(views)
+    }
+
+    pub async fn list_outputs(&mut self) -> io::Result<Vec<Output>> {
+        let message = MsgTemplate {
+            method: "window-rules/list-outputs".to_string(),
+            data: None,
         };
 
-        Ok(filtered_views)
+        let response = self.send_json(&message).await?;
+        let outputs: Vec<Output> = serde_json::from_value(response)?;
+
+        Ok(outputs)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-mod ipc;
-use ipc::WayfireSocket;
 use std::io;
+mod ipc;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut wayfire_socket = WayfireSocket::connect().await?;
-    let views = wayfire_socket.list_views(true).await?;
+    let mut wayfire_socket = ipc::WayfireSocket::connect().await?;
+    let views = wayfire_socket.list_views().await?;
+    let outputs = wayfire_socket.list_outputs().await?;
     for view in views {
         println!("{:?}", view);
     }
+    for output in outputs {
+        println!("{:?}", output);
+    }
+
     Ok(())
 }
